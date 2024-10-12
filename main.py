b921272bd354317cc1a6069658005c46d4222509
Eddy602 <edwardodjidjaaa@gmail.com>
2024-10-12 15:51:32 +0300

    Loops

---
+++ main.py
@@ -0,0 +1,28 @@
+    #activity1
+# word="school"
+# for char in word:
+#     print(char)
+# for i in range(0,11):
+#     print(i)
+
+    #activity 2
+# number=int(input("enter a number"))
+# for i in range(0,11):
+#     print(number,"x",i,"=",number*i)
+
+         #activity 3
+# sumno=sum(range(0,201))
+# print(sumno)
+
+        #activity 4
+# number=int(input("enter to check if number is prime or not"))
+
+# flag=False
+# if number>1:
+#     for i in range(2,number):
+#         if(number%i==0):
+#             break
+# if flag:
+#     print("not prime")
+# else:
+#     print("prime")            
